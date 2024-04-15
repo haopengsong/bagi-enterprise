ad6c31cac0b4257fb93bbd3bdac147335bd4ef3e
haopengsong <jsong2018@icloud.com>
2024-04-15 16:06:26 +0800

    version update

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -17,8 +17,8 @@
  */
 export const Brand = {
   Title: {
-    Base: 'xChat V2.0.3',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'xChat V2.0.3',
+    Base: 'xChat V2.0.4',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'xChat V2.0.4',
   },
   Meta: {
     Description: 'Launch xGLM to unlock the full potential of AI',
