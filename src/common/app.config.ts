7cbf06583f2254970f4140b10961fed0040e212d
haopengsong <jsong2018@icloud.com>
2024-05-30 15:10:16 +0800

    shrinking chat length

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -17,8 +17,8 @@
  */
 export const Brand = {
   Title: {
-    Base: 'xChat V3.3.2',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'xChat V3.3.2',
+    Base: 'xChat V3.3.3',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'xChat V3.3.3',
   },
   Meta: {
     Description: 'Launch xGLM to unlock the full potential of AI',
